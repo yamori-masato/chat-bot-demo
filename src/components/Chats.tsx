204b0d13f15ec31062e9b1beb254fa578837127f
yamori-masato <yamorimasato@gmail.com>
2021-03-06 04:00:17 +0900

    Chatsコンポーネントにスタイルをあてる

--- src/components/Chats.tsx
+++ src/components/Chats.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 import List from '@material-ui/core/List'
 import { Chat } from './index'
 
@@ -7,19 +7,21 @@ type Props = {
   chats: Chats
 }
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-    maxWidth: '36ch',
-    backgroundColor: theme.palette.background.paper,
-  }
-}))
+const useStyles = makeStyles((theme: Theme) => (
+  createStyles({
+    chats: {
+      height: 400,
+      padding: 0,
+      overflow: 'auto'
+    }
+  })
+))
 
 const Chats: React.FC<Props> = (props) => {
   const classes = useStyles()
 
   return (
-    <List className={classes.root}>
+    <List className={classes.chats}>
       {props.chats.map((chat, index) => {
         return <Chat text={chat.text} type={chat.type} key={index.toString()} />
       })}
