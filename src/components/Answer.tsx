bc30c63316a7ecee991d09c6f322ed66d989b5b4
yamori-masato <yamorimasato@gmail.com>
2021-03-06 03:56:57 +0900

    Answerコンポーネントにスタイルをあてる

--- src/components/Answer.tsx
+++ src/components/Answer.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 
 type Props = {
@@ -7,9 +8,30 @@ type Props = {
   select: (selectedAnswer: string, nextQuestionId: string) => void
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    button: {
+      borderColor: '#FFB549',
+      color: '#FFB549',
+      fontWeight: 600,
+      marginBottom: '8px',
+      '&:hover': {
+        backgroundColor: '#FFB549',
+        color: '#fff'
+      }
+    }
+  })
+)
+
 const Answer: React.FC<Props> = (props) => {
+  const classes = useStyles()
+  console.log(classes)
   return (
-    <Button variant="contained" color="primary" onClick={() => props.select(props.content, props.nextId)}>
+    <Button
+      className={classes.button}
+      variant="outlined"
+      onClick={() => props.select(props.content, props.nextId)}
+    >
       {props.content}
     </Button>
   )
